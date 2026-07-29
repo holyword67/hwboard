ef4fb6918e2e0c4ec8c82b82448cbd7d27df22d5
holyword67 <holyword67@gmail.com>
2026-07-29 14:56:49 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // ============================================================
 // src/main.rs
 // ============================================================
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 mod app;
 mod clipboard;
 mod gpu;
